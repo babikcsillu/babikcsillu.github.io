81309e433f9b263d91ea732ce7e2e61aa2ac66cc
babikcsillu <babikcsillu@gmail.com>
2018-09-02 22:06:25 +0200

    site 2.0 release re-try

---
+++ js/doslick.js
@@ -0,0 +1,24 @@
+$('.slickthis').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  swipe: true,
+  cssEase: 'linear',
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  touchMove: true,
+  accessibility: true
+});
+
+
+$('.slickthisvideo').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  swipe: true,
+  cssEase: 'linear',
+  slidesToShow: 2,
+  adaptiveHeight: true,
+  touchMove: true
+});
+
